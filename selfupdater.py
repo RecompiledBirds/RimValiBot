47dc38773b200c0ba1f9b465adbc2819d07fdad7
NesiAvali <fiveggamer@gmail.com>
2022-06-25 22:25:58 -0400

    Update selfupdater.py

--- selfupdater.py
+++ selfupdater.py
@@ -3,26 +3,29 @@ import shutil
 import os
 import data
 import glob
-import stat
 from dotenv import load_dotenv
 load_dotenv()
 path=f"{data.directory}\downloaded"
 
 
 def selfupdate():
-    if(os.path.exists(path)):
-        pardir = os.path.abspath(os.path.join(path, os.path.pardir))
-        os.chmod(pardir, stat.S_IWRITE )
-        os.chmod(path, stat.S_IWRITE )
-        os.unlink(path)
-        for file in files:
-            print(file)
-      
-        shutil.rmtree(path)
+  if(os.path.exists(path)):
+    shutil.rmtree(path)
 
-    git.Repo.clone_from(os.getenv("REPO"),path)
-    shutil.rmtree(f"{path}/.git")
-    shutil.copy(path,data.directory)
+  git.Repo.clone_from(os.getenv("REPO"),path)
+  shutil.rmtree(f"{path}/.git")
+ 
+  files = glob.glob(f"{path}/*")
+  for file in files:
+    fileName=file.replace(path,"")
+    fp=f"{data.directory}/{fileName}"
+    if os.path.exists(fp):
+      if os.path.isdir(fp):
+        shutil.rmtree(fp)
+      else:
+        os.remove(fp)
+    print(f"Updaing: {fileName}")
+    shutil.move(file,data.directory)
 
 
 
